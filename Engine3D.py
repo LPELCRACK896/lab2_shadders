77490ab65b32e3e0d3f61f39c7d93292905cd8f8
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-08-16 18:04:04 -0600

    initial commit

---
+++ Engine3D.py
@@ -0,0 +1,44 @@
+from gl import Renderer, color, V3, V2
+from texture import Texture
+from shaders import flat, unlit, gourad, toon, glow, textureBlend
+
+width = 960
+height = 540
+
+rend = Renderer(width, height)
+
+rend.dirLight = V3(1,0,0)
+
+rend.active_texture = Texture("models/model.bmp")
+rend.active_shader = gourad
+
+rend.glLoadModel("models/model.obj",
+                 translate = V3(0, 0, -10),
+                 scale = V3(3,3,3),
+                 rotate = V3(90,0,0))
+
+
+
+#rend.active_texture = Texture("models/model.bmp")
+#rend.active_shader = gourad
+#rend.glLoadModel("models/model.obj",
+#                 translate = V3(-4, 0, -10),
+#                 scale = V3(3,3,3),
+#                 rotate = V3(0,0,0))
+
+#rend.active_shader = toon
+#rend.glLoadModel("models/model.obj",
+#                 translate = V3(0, 0, -10),
+#                 scale = V3(3,3,3),
+#                 rotate = V3(0,0,0))
+
+#rend.active_shader = glow
+#rend.glLoadModel("models/model.obj",
+#                 translate = V3(4, 0, -10),
+#                 scale = V3(3,3,3),
+#                 rotate = V3(0,0,0))
+
+
+
+rend.glFinish("output.bmp")
+
